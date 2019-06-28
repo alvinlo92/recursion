2962722d9666517d7c292b2d4d30792c8c7be2f9
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2019-06-28 12:20:04 -0700

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,29 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === "string") {
+  	return '"' + obj + '"';
+  }
+  if (obj === null) {
+  	return 'null';
+  }
+  if (typeof obj === "number" || typeof obj === "boolean") {
+  	return obj.toString();
+  }
+  if (Array.isArray(obj) === true) {
+    var arr = [];
+    for (var i = 0; i < obj.length; i++) {
+      arr.push(stringifyJSON(obj[i]));
+    }
+    return '[' + arr + ']';
+  }
+  if (typeof obj === 'object') {
+  	var newArr = [];
+    for (var prop in obj) {
+    	if (typeof obj[prop] !== "function" && obj[prop] !== undefined) {
+    	  newArr.push(stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]));
+      }
+    }
+    return '{' + newArr + '}';
+  }
 };
